52d663c799f4f96f793c94da4cdd0732a585375d
konrad <lorenz>
2021-06-29 21:11:32 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="metovhooks",
-    version="0.1.0",
+    version="0.1.1",
     description="My personal git hooks.",
     url="https://github.com/metov/metovhooks",
     long_description=Path("README.md").read_text(),
