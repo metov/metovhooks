8c603e993ccde30a56e19081f16cb696e7cff574
Azat Akhmetov <azatinfo@yandex.com>
2021-09-16 23:51:58 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="metovhooks",
-    version="0.1.3",
+    version="0.1.4",
     description="My personal git hooks.",
     url="https://github.com/metov/metovhooks",
     long_description=Path("README.md").read_text(),
